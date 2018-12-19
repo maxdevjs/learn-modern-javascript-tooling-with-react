a370e25b7ccbec6c666d5f580c060aa363f67683
Massimiliano Giroldi <max.devjs@gmail.com>
2018-12-19 19:50:42 -0200

    30_react_axe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App'
 import DefaultErrorBounday from './DefaultErrorBoundary'
 import './style.css'
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDom, 1000)
+}
+
 ReactDom.render(
   <React.StrictMode>
     <DefaultErrorBounday>
