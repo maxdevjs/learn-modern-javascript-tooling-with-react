8b01ddf25e848f0e771195afd266913a93518512
Massimiliano Giroldi <max.devjs@gmail.com>
2018-12-18 23:15:03 -0200

    14_proposed_syntax_babel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
   render() {
-    debugger;
-    return <h1>Hello w0rld - from webpack dev server</h1>;
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button
+          onClick={() => this.setState(state => ({count: state.count + 1}))}>
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({count: state.count - 1}))}>
+          -
+        </button>
+      </div>
+    );
   }
 }
 
