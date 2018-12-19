b1b2a341fe02c6fe1694dfd4530ff6052ca0117b
Massimiliano Giroldi <max.devjs@gmail.com>
2018-12-19 19:38:19 -0200

    29_prop_types

--- src/DefaultErrorBoundary.js
+++ src/DefaultErrorBoundary.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
 export default class DefaultErrorBoundary extends React.Component {
   state = {
@@ -9,10 +10,13 @@ export default class DefaultErrorBoundary extends React.Component {
     return { isError: true }
   }
 
+  static propTypes = {
+    children: propTypes.node.isRequired
+  }
+
   render() {
     const { isError } = this.state
-    // const { children } = this.props
-    // return isError ? <div>Something went wrong</div> : children
-    return isError ? <div>Something went wrong</div> : null
+    const { children } = this.props
+    return isError ? <div>Something went wrong</div> : children
   }
 }
