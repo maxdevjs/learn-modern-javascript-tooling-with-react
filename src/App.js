3d84845fb4a5c3373263dfa36d666fbff861c934
Massimiliano Giroldi <max.devjs@gmail.com>
2018-12-19 22:21:40 -0200

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
-
 class App extends React.Component {
-  state = {
-    count: 0
-  }
-
-  // UNSAFE_componentWillMount() {
-  //   console.log('This lifecycle hook should be avoided')
-  // }
-
   render() {
-    // throw new Error('bo0om!')
-    const { count } = this.state
     return (
       <div>
         <h1>Hello World</h1>
-        <h2 className={count > 10 ? 'warning' : null}>
-          Count: {this.state.count}
-        </h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
       </div>
     )
   }
